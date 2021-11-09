5deb0f1cbd13f59f8bc85628b02ed2e5d3d7b8fa
celestinacoded <you@celestinaluv6@gmail.com>
2021-11-09 16:41:16 +0100

    Apartment project

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div style={{color: "black"}}>Header Navigation</div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header/Header'
+
+const HomePage = () => {
+    return (
+        <div>
+           <div>
+               <Header />
+          </div> 
+        </div>
+    )
+}
+
+export default HomePage
